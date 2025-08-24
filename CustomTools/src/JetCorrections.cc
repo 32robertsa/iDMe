0a7827eae83d9e4c33e06c12dcdca83942be47a6
Reshma Menon R <pct3kd@virginia.edu>
2025-08-24 14:22:02 -0500

    Added values for 2022

--- CustomTools/src/JetCorrections.cc
+++ CustomTools/src/JetCorrections.cc
@@ -22,7 +22,7 @@ bool JetCorrections::passJetID(const reco::PFJet &jet, int idx) {
     auto chargedMult = jet.chargedMultiplicity();
     auto neutMult = jet.neutralMultiplicity();
 
-    bool passID = false;
+    bool passID = true;
 
     if (year == 2016) {
         if (abs(eta) <= 2.4) {
@@ -38,18 +38,18 @@ bool JetCorrections::passJetID(const reco::PFJet &jet, int idx) {
             passID = (neutHadFrac > 0.2) && (neutEmFrac < 0.9) && (neutMult > 10);
         }
     }
-    else if ((year == 2017) || (year == 2018)) {
+    else if ((year == "2017") || (year == "2018") || (year == "2022") || (year == "2023") ){
         if (abs(eta) <= 2.6) {
-            passID = (neutHadFrac < 0.9) && (neutEmFrac < 0.9) && (nConstit > 1) && (chargedHadFrac > 0) && (chargedMult > 0);
+            passID = (neutHadFrac < 0.90) && (neutEmFrac < 0.90) && (nConstit > 1) && (muonFrac < 0.8) && (chargedHadFrac > 0.01) && (chargedMult > 0) && (chEmFrac < 0.80);
         }
-        else if ((abs(eta) >= 2.6) && (abs(eta) <= 2.7)) {
-            passID = (neutHadFrac < 0.9) && (neutEmFrac < 0.99) && (chargedMult > 0);
+        else if ((abs(eta) > 2.6) && (abs(eta) <= 2.7)) {
+            passID = (neutHadFrac < 0.9) && (neutEmFrac < 0.99) && (muonFrac < 0.8) && (chEmFrac < 0.8);
         }
-        else if ((abs(eta) >= 2.7) && (abs(eta) <= 3.0)) {
-            passID = (neutEmFrac < 0.99) && (neutEmFrac > 0.01) && (neutMult > 1);
+        else if ((abs(eta) > 2.7) && (abs(eta) <= 3.0)) {
+            passID = (neutHadFrac < 0.9999);
         }
         else if ((abs(eta) >= 3.0) && (abs(eta) < 5.0)) {
-            passID = (neutHadFrac > 0.2) && (neutEmFrac < 0.9) && (neutMult > 10);
+            passID = (neutEmFrac < 0.9) && (neutMult > 2);
         }
     }
 
